bd8c44697218ddb00b478b1c79c652c9bb82b4fb
Ron Bierman <ron.bierman@sbisecsol.com>
2021-04-26 12:25:40 -0700

    task: Cleanup the `convertSwaggerToMarkdown` script a bit

--- scripts/convertSwaggerToMarkdown.js
+++ scripts/convertSwaggerToMarkdown.js
@@ -5,7 +5,14 @@ const YAML = require('yaml')
 const options = {
   codeSamples: true,
   httpsnippet: false,
-  language_tabs: [{csharp: "C#"}, {shell: "Shell"}, {http: "HTTP"}, {'javascript': 'JavaScript'}, {'javascript--node': 'Node.JS'}, {ruby: "Ruby"}],
+  language_tabs: [
+    { 'csharp': "C#" },
+    { 'shell': "Shell" },
+    { 'http': "HTTP" },
+    { 'javascript': 'JavaScript' },
+    { 'javascript--node': 'Node.JS' },
+    { 'ruby': 'Ruby' }
+  ],
   templateCallback: function (templateName, stage, data) {
     return data;
   },
@@ -13,22 +20,24 @@ const options = {
   search: true,
   sample: true,
   discovery: false,
-  includes: ['introduction', 'flows'],
+  includes: [
+    'introduction',
+    'flows'
+  ],
   shallowSchemas: false,
   tocSummary: false,
   headings: 2,
   yaml: false,
   resolve: false,
   toc_footers: [{ url: "https://snapchance.no", description: "SnapChance Norway" }]
-
 }
 
+// TODO: We might want to consider moving this repo to a subdir of the api project or getting this file in some other way
 const fileData = fs.readFileSync('../fotolotto-snapchance-api/public/apiDocs.yaml', 'utf8');
 const data  = YAML.parse(fileData);
 
 widdershins.convert(data, options)
   .then(markdownOutput => {
-    // markdownOutput contains the converted markdown
     fs.writeFileSync('./source/index.html.md', markdownOutput, 'utf8');
   })
   .catch(err => {
